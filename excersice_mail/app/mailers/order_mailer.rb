bca6e02210e6a0c02754bc7c0b80efebbad000c8
Yashkheni007 <yashkheni007@gmail.com>
2024-04-29 12:57:42 +0530

    update mail subject

--- excersice_mail/app/mailers/order_mailer.rb
+++ excersice_mail/app/mailers/order_mailer.rb
@@ -1,6 +1,6 @@
 class OrderMailer < ApplicationMailer
   def order_confirmation_email(user_email)
     @user_email = user_email
-    mail(to: user_email, subject: 'Order Confirmation')
+    mail(to: user_email, subject: 'testing mail')
   end
 end
